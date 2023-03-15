7eb37c1046a83d12af6f65b7c0fc75f6d206fa0f
Georgina <faithwangare750@gmail.com>
2023-03-15 22:02:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // multiply num1 and num2
+console.log(multiply); // output: 62
+
+let random = Math.floor(Math.random() * 100) + 1; // generate a random integer between 1 and 100
+console.log(random);
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4; // calculate remainder of num3 divided by num4
+console.log(mod); // output: 4
+
+let numbers = [8, 20, 3, 12, 6, 18, 4, 15];
+let max = Math.max(...numbers); // find the maximum value in the array
+console.log(max); // output: 20
